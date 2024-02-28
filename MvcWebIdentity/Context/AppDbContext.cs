980b241af0eb60eb38395b3fb1202de2d0e68019
Rodrigo Peixoto <rodrigo.peixoto@outlook.com>
2024-02-28 15:37:08 -0300

    Adicionar arquivos de projeto.

---
+++ MvcWebIdentity/Context/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcWebIdentity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace MvcWebIdentity.Context;
+
+public class AppDbContext : IdentityDbContext
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+        
+    public DbSet<Aluno> Alunos { get; set; }
+
+    internal static object CreateBuilder(string[] args)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Aluno>().HasData(
+            new Aluno
+            {
+                AlunoId = 1,
+                Nome = "RodPeixoto",
+                Email = "rodpeixoto@outlook.com",
+                Idade = 23,
+                Curso = "Filosofia"
+
+            });
+    }
+}
